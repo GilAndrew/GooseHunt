7f45cc7161f6a7751e9758926582d6b6c7e2cf9d
GilAndrew <gilfandrew@gmail.com>
2020-11-25 11:11:38 -0700

    Revert "Goose Movement, Shooting, more Robust Factory"
    This reverts commit fb9e66e29df0908eb369d1d71dc2d705c9ac8022.

--- Main.gd
+++ Main.gd
@@ -1,30 +1,19 @@
 extends Node
 
-var gooseSpawner = load("res://goose_spawner.gd").new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# NOTE TO SELF, Main.gd WILL CALL THE GOOSESPAWNER (WHICH SHOULD RETURN AN INSTANCE OF GOOSE)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func new_game():
-	var goose_inst = gooseSpawner.generateGoose("easy")
-	#var goose_inst = goose.instance()
-	
-	add_child(goose_inst)
-	goose_inst.connect("shot", self, "_on_goose_shot")
-	var startPos = Position2D.new()
-	startPos.position.x = 200
-	startPos.position.y = 100
-	goose_inst.start(startPos.position)
-	# Some basic movement code, direction  and the rotation stuff will be taken into factor when we come up with movement paths
-	#var direction = (PI / 2)
-	goose_inst.linear_velocity = Vector2(rand_range(100, 250), 0)
-	#linear_velocity = linear_velocity.rotated(PI / 2)
-
-func _on_goose_shot():
-	print("The goose was shot, says main")
