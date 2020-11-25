fb9e66e29df0908eb369d1d71dc2d705c9ac8022
GilAndrew <gilfandrew@gmail.com>
2020-11-25 11:05:46 -0700

    Goose Movement, Shooting, more Robust Factory

--- goose.gd
+++ goose.gd
@@ -1,21 +1,21 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+class_name Goose
+
+signal shot
+
 var spawner
 var goose
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawner = GooseSpawner.new()
-	goose = spawner.generateGoose("easy")
-	#add_child(goose) # this will be moved to "main" node/scene later
-	#goose.start(200,100)
-	var startPos = Position2D.new()
-	startPos.position.x = 200
-	startPos.position.y = 100
-	start(startPos.position)
+	pass
+	#spawner = GooseSpawner.new()
+	#goose = spawner.generateGoose("easy")
+	#var startPos = Position2D.new()
+	#startPos.position.x = 200
+	#startPos.position.y = 100
+	#start(startPos.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,7 +25,12 @@ func _ready():
 func start(pos) :
 	position = pos
 	show()
-	
+
+func _input(event):
+	if event is InputEventMouseButton :
+		if (event.button_index == BUTTON_LEFT and event.pressed):
+			hide()
+			emit_signal("shot")
 
 
 func _on_VisibilityNotifier2D_screen_exited():
