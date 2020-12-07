2e4fbf5781a5f30595c3cda4225f9468c3813e41
GilAndrew <gilfandrew@gmail.com>
2020-12-07 11:09:19 -0700

    Delete ScoreScreenScript.gd

--- ScoreScreenScript.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready():
-	$returnButton.connect("pressed", self, "_on_Button_Pressed", [$returnButton.scene_to_load])
-	
-	
-func _on_Button_Pressed(scene_to_load):
-	get_tree().change_scene(scene_to_load)
